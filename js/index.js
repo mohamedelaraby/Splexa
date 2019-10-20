d2bf919252e5a314fc6bbb7cf7bdb041f79c6194
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-10-20 07:44:44 +0200

    Finish splexa

--- js/index.js
+++ js/index.js
@@ -8,3 +8,20 @@
 $(document).ready(function(){
      $('#year').text(new Date().getFullYear());
   });
+
+  // Init scroll spy
+$('body').scrollspy({target:'#main-nav'});
+
+// Smooth scrolling
+$('#main-nav a').on('click', function(event){
+    if(this.hash !== ""){
+        event.preventDefault();
+        const hash = this.hash;
+        $('html, body').animate({
+            scrollTop:$(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        });
+    }
+});
+   
